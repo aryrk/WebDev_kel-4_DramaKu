38acd1cd6ad82ac1992c8a1438ef3756b84a7906
aryrk <amanwhohave@gmail.com>
2024-08-31 13:24:29 +0700

    [feat] implement responsive on search page

--- view/src/pages/SearchResultPage.jsx
+++ view/src/pages/SearchResultPage.jsx
@@ -1,10 +1,58 @@
 import React, { useEffect } from "react";
 
-
-
 import { useGlobalState } from "../components/GlobalStateContext";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col, Image } from "react-bootstrap";
+
+function MovieCard(props) {
+  const { src, title, year, genre, cast, views } = props;
+  return (
+    <Col sm="6" className="mb-4 ps-0 pe-0 ps-md-1 pe-md-1">
+      <div className="d-flex align-items-start">
+        <Container>
+          <Row>
+            <Col sm="auto" className="p-0">
+              <Image
+                className="w-sm-100 w-md-100 img-fluid rounded img_cover"
+                src={src}
+                fluid
+                loading="lazy"
+                style={{
+                  width: "150px",
+                  height: "150px",
+                }}
+              />
+            </Col>
+            <Col className="p-0">
+              <div className="ms-4 ms-md-3 mt-3 mt-md-0">
+                <h5 className="mb-1">{title}</h5>
+                <p className=" mb-1">{year}</p>
+                <p className=" mb-1">
+                  {genre.map((title, i) => (
+                    <span key={i}>
+                      {title}
+                      {i < genre.length - 1 ? ", " : ""}
+                    </span>
+                  ))}
+                </p>
+                <p className=" mb-1">
+                  {cast.map((title, i) => (
+                    <span key={i}>
+                      {title}
+                      {i < cast.length - 1 ? " • " : ""}
+                    </span>
+                  ))}
+                </p>
+                <p className="">{views} views</p>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </Col>
+  );
+}
 
 const SearchResultPage = () => {
   const { setShowNavigation, setShowFooter } = useGlobalState();
@@ -15,155 +63,70 @@ const SearchResultPage = () => {
   }, [setShowNavigation]);
   return (
     <>
-      <section className="py-5 text-wheat">
-        <div className="container">
-          <div className="text-center mb-5">
-            <h2 className="display-4 font-weight-bold">
-              Searched/Tagged with "outsider"
-            </h2>
-          </div>
-
-          <div className="row justify-content-end">
-            <div className="col-6 mb-4">
-              <div className="d-flex align-items-start">
-                <img
-                  className="img-fluid rounded"
-                  src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/action-mistery-movie-poster-design-template-2ec690d65c22aa12e437d765dbf7e4af_screen.jpg?ts=1680854635"
-                  alt="Blog 1"
-                  style={{
-                    width: "150px",
-                    height: "150px",
-                    objectFit: "cover",
-                  }}
-                />
-                <div className="ms-3">
-                  <h5 className="mb-1">The Outsider</h5>
-                  <p className=" mb-1">2020</p>
-                  <p className=" mb-1">Crime, Drama, Horror</p>
-                  <p className=" mb-1">
-                    Ben Mendelsohn • Bill Camp • Jeremy Bobb
-                  </p>
-                  <p className="">100 views</p>
-                </div>
-              </div>
+      <section className="pt-3 pt-md-4 text-wheat">
+        <center>
+          <Container className="p-0 m-0 w-100 text-start">
+            <div className="text-center mb-4 mb-md-5">
+              <p className="fs-7 fs-md-5 font-weight-bold">
+                Searched/Tagged with "outsider"
+              </p>
             </div>
 
-            <div className="col-6 mb-4">
-              <div className="d-flex align-items-start">
-                <img
-                  className="img-fluid rounded"
-                  src="https://m.media-amazon.com/images/M/MV5BMjAxMjUxMjAzOF5BMl5BanBnXkFtZTgwNjQzOTc4NDM@._V1_.jpg"
-                  alt="Blog 2"
-                  style={{
-                    width: "150px",
-                    height: "150px",
-                    objectFit: "cover",
-                  }}
-                />
-                <div className="ms-3">
-                  <h5 className="mb-1">The Outsider</h5>
-                  <p className=" mb-1">2022</p>
-                  <p className=" mb-1">Action, Crime, Drama</p>
-                  <p className=" mb-1">
-                    Jared Leto • Tadanobu Asano • Kippei Shîna
-                  </p>
-                  <p className="">78 views</p>
-                </div>
-              </div>
-            </div>
+            <Row className="m-0">
+              <MovieCard
+                src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/action-mistery-movie-poster-design-template-2ec690d65c22aa12e437d765dbf7e4af_screen.jpg?ts=1680854635"
+                title="The Outsider"
+                year="2022"
+                genre={["Action", "Crime", "Drama"]}
+                cast={["Jared Leto", "Tadanobu Asano", "Kippei Shîna"]}
+                views="78"
+              />
 
-            <div className="col-6 mb-4">
-              <div className="d-flex align-items-start">
-                <img
-                  className="img-fluid rounded"
-                  src="https://m.media-amazon.com/images/M/MV5BMTIzMjczMjg3M15BMl5BanBnXkFtZTcwODkxMDk0MQ@@._V1_FMjpg_UX1000_.jpg"
-                  alt="Blog 1"
-                  style={{
-                    width: "150px",
-                    height: "150px",
-                    objectFit: "cover",
-                  }}
-                />
-                <div className="ms-3">
-                  <h5 className="mb-1">The Outsider</h5>
-                  <p className=" mb-1">2022</p>
-                  <p className=" mb-1">Drama, Romance, Westem</p>
-                  <p className=" mb-1">
-                    Tim Daly • Naomi Watts • Keith Carradine
-                  </p>
-                  <p className="">39 views</p>
-                </div>
-              </div>
-            </div>
-            <div className="col-6 mb-4">
-              <div className="d-flex align-items-start">
-                <img
-                  className="img-fluid rounded"
-                  src="https://m.media-amazon.com/images/M/MV5BY2E4Njk4N2UtZWFhOS00NzczLWFmNDgtMzdhMjFlNTZjMmVhL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_QL75_UX190_CR0,4,190,281_.jpg"
-                  alt="Blog 1"
-                  style={{
-                    width: "150px",
-                    height: "150px",
-                    objectFit: "cover",
-                  }}
-                />
-                <div className="ms-3">
-                  <h5 className="mb-1">The Outsiders</h5>
-                  <p className=" mb-1">1983</p>
-                  <p className=" mb-1">Teen Drama, Crime, Drama</p>
-                  <p className=" mb-1">
-                    C. Thomas Howell • Matt Dillon • Ralph Macchio
-                  </p>
-                  <p className="">19 views</p>
-                </div>
-              </div>
-            </div>
-            <div className="col-6 mb-4">
-              <div className="d-flex align-items-start">
-                <img
-                  className="img-fluid rounded"
-                  src="https://m.media-amazon.com/images/M/MV5BMTQwNDMxMDc2NF5BMl5BanBnXkFtZTgwNzk5MTI5MDE@._V1_QL75_UX190_CR0,2,190,281_.jpg"
-                  alt="Blog 1"
-                  style={{
-                    width: "150px",
-                    height: "150px",
-                    objectFit: "cover",
-                  }}
-                />
-                <div className="ms-3">
-                  <h5 className="mb-1">The Outsider</h5>
-                  <p className="mb-1">2014</p>
-                  <p className="mb-1">Action, Adventure, Crime</p>
-                  <p className="mb-1">
-                    Craig Fairbrass • James Caan • Jason Patric
-                  </p>
-                  <p className="">18 views</p>
-                </div>
-              </div>
-            </div>
-            <div className="col-6 mb-4">
-              <div className="d-flex align-items-start">
-                <img
-                  className="img-fluid rounded"
-                  src="https://i.mydramalist.com/0wxjQ4_4f.jpg"
-                  alt="Blog 1"
-                  style={{
-                    width: "150px",
-                    height: "150px",
-                    objectFit: "cover",
-                  }}
-                />
-                <div className="ms-3">
-                  <h5 className="mb-1">The Outsider</h5>
-                  <p className=" mb-1">2023</p>
-                  <p className=" mb-1">Drama</p>
-                  <p className=" mb-1">Yuqi Zhang • Weiguang Gao • Shi Shi</p>
-                  <p className="">9 views</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+              <MovieCard
+                src="https://m.media-amazon.com/images/M/MV5BMjAxMjUxMjAzOF5BMl5BanBnXkFtZTgwNjQzOTc4NDM@._V1_.jpg"
+                title="The Outsider"
+                year="2022"
+                genre={["Action", "Crime", "Drama"]}
+                cast={["Jared Leto", "Tadanobu Asano", "Kippei Shîna"]}
+                views="78"
+              />
+
+              <MovieCard
+                src="https://m.media-amazon.com/images/M/MV5BMTIzMjczMjg3M15BMl5BanBnXkFtZTcwODkxMDk0MQ@@._V1_FMjpg_UX1000_.jpg"
+                title="The Outsider"
+                year="2022"
+                genre={["Action", "Crime", "Drama"]}
+                cast={["Jared Leto", "Tadanobu Asano", "Kippei Shîna"]}
+                views="78"
+              />
+
+              <MovieCard
+                src="https://m.media-amazon.com/images/M/MV5BY2E4Njk4N2UtZWFhOS00NzczLWFmNDgtMzdhMjFlNTZjMmVhL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_QL75_UX190_CR0,4,190,281_.jpg"
+                title="The Outsider"
+                year="2022"
+                genre={["Action", "Crime", "Drama"]}
+                cast={["Jared Leto", "Tadanobu Asano", "Kippei Shîna"]}
+                views="78"
+              />
+              <MovieCard
+                src="https://m.media-amazon.com/images/M/MV5BMTQwNDMxMDc2NF5BMl5BanBnXkFtZTgwNzk5MTI5MDE@._V1_QL75_UX190_CR0,2,190,281_.jpg"
+                title="The Outsider"
+                year="2022"
+                genre={["Action", "Crime", "Drama"]}
+                cast={["Jared Leto", "Tadanobu Asano", "Kippei Shîna"]}
+                views="78"
+              />
+              <MovieCard
+                src="https://i.mydramalist.com/0wxjQ4_4f.jpg"
+                title="The Outsider"
+                year="2022"
+                genre={["Action", "Crime", "Drama"]}
+                cast={["Jared Leto", "Tadanobu Asano", "Kippei Shîna"]}
+                views="78"
+              />
+            </Row>
+          </Container>
+        </center>
       </section>
     </>
   );

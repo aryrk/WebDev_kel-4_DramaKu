41bc90723ca28c78c2189779af918f953ae6c164
HasnaFitriyaniK <hasna.fitriyani.23@gmail.com>
2024-09-01 16:21:31 +0700

    navbar responsive

--- view/src/components/Navigation.jsx
+++ view/src/components/Navigation.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Container,
   Navbar,
@@ -6,167 +6,157 @@ import {
   Form,
   Button,
   NavDropdown,
-  Row,
-  Col,
   Image,
 } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./componentsStyle/CustomNavbar.css";
+import "./componentsStyle/Navbar.css";
 import { withConfig } from "../Config";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-// import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const CustomNavbar = ({ config }) => {
+  const [show, setShow] = useState(false);
+
   useEffect(() => {
     document.title = `Page 1 - ${config.short_name}`;
   }, [config]);
+
+  const handleToggle = () => setShow(!show);
 
   return (
     <>
       {/* Navbar Atas */}
       <Navbar sticky="top" expand="lg" className="navbar-custom">
         <Container>
-          {/* Navbar Toggle for Mobile */}
-          <Navbar.Toggle aria-controls="top-navbar-nav" className="d-lg-none" />
+          {/* Navbar Toggle for Desktop */}
+          <Navbar.Toggle
+            aria-controls="top-navbar-nav"
+            className="custom-toggler d-none d-lg-block"
+          />
 
-          {/* Brand and Search Icon for Mobile */}
-          <Navbar.Brand className="mx-auto d-lg-none">
-            <Image
-              src={config.logo_png}
-              width={30}
-              height={30}
-              loading="lazy"
-              className="img_contain d-inline-block align-top "
-            />
-            {config.short_name}
-          </Navbar.Brand>
-          <Nav className="ms-auto d-lg-none">
-            <Nav.Link href="#search">
+          {/* Navbar Collapse for Mobile */}
+          <div className="d-lg-none d-flex justify-content-between align-items-center w-100">
+            {/* Toggle for Mobile */}
+            <Button
+              variant="link"
+              onClick={handleToggle}
+              className="p-0 custom-toggler"
+              aria-controls="top-navbar-nav"
+              aria-expanded={show}
+            >
               <FontAwesomeIcon
                 icon={faMagnifyingGlass}
                 className="text-white"
               />
-            </Nav.Link>
-          </Nav>
+            </Button>
 
-          {/* Navbar Collapse for Desktop */}
-          <Navbar.Collapse id="top-navbar-nav" className="d-block">
-            <Container>
-              <Row>
-                <Col>
-                  <Nav className="me-auto d-none d-lg-flex align-items-center">
-                    <Navbar.Brand className="me-3">
-                      <Image
-                        src={config.logo_png}
-                        width={30}
-                        height={30}
-                        loading="lazy"
-                        className="d-inline-block align-top img_contain"
-                      />
-                      {config.short_name}
-                    </Navbar.Brand>
-                    <Nav.Link href="#indonesia" className="navbar-custom">
-                      Indonesia
-                    </Nav.Link>
-                    <Nav.Link href="#japan" className="navbar-custom">
-                      Japan
-                    </Nav.Link>
-                    <Nav.Link href="#korea" className="navbar-custom">
-                      Korea
-                    </Nav.Link>
-                    <Nav.Link href="#spanyol" className="navbar-custom">
-                      Spanyol
-                    </Nav.Link>
-                    <Nav.Link href="#inggris" className="navbar-custom">
-                      Inggris
-                    </Nav.Link>
-                    <Nav.Link href="#amerika" className="navbar-custom">
-                      Amerika
-                    </Nav.Link>
-                    <Nav.Link href="#thailand" className="navbar-custom">
-                      Thailand
-                    </Nav.Link>
-                    <Nav.Link href="#filiphina" className="navbar-custom">
-                      Filiphina
-                    </Nav.Link>
-                    <Nav.Link href="#taiwan" className="navbar-custom">
-                      Taiwan
-                    </Nav.Link>
-                  </Nav>
-                </Col>
-                <Col className="align-items-center override-flex justify-content-center">
-                  <Form className="d-none d-lg-flex w-auto">
-                    <Form.Control
-                      type="text"
-                      placeholder="Search"
-                      aria-label="Search"
-                      style={{ maxWidth: "270px" }}
-                    />
-                    <Nav className="d-none d-lg-flex align-items-center ms-2">
-                      <Nav.Link
-                        href="#search"
-                        className="d-flex align-items-center search-icon"
-                      >
-                        <FontAwesomeIcon icon={faMagnifyingGlass} />
-                      </Nav.Link>
-                    </Nav>
-                  </Form>
-                </Col>
-              </Row>
-            </Container>
-          </Navbar.Collapse>
+            {/* Brand */}
+            <Navbar.Brand className="d-flex align-items-center mx-auto">
+              <Image
+                src={config.logo_png}
+                width={30}
+                height={30}
+                loading="lazy"
+                className="img_contain d-inline-block align-top"
+              />
+              <div>{config.short_name}</div>
+            </Navbar.Brand>
 
-          {/* Navbar Collapse for Mobile */}
-          {/* <Navbar.Collapse id="top-navbar-nav" className="d-lg-none">
-            <Nav className="flex-column">
-              <NavDropdown
-                title="Countries"
-                id="countries-dropdown"
-                className="d-lg-none"
-              >
-                <NavDropdown.Item href="#indonesia">Indonesia</NavDropdown.Item>
-                <NavDropdown.Item href="#japan">Japan</NavDropdown.Item>
-                <NavDropdown.Item href="#korea">Korea</NavDropdown.Item>
-                <NavDropdown.Item href="#korea">Spanyol</NavDropdown.Item>
-                <NavDropdown.Item href="#korea">Inggris</NavDropdown.Item>
-                <NavDropdown.Item href="#korea">Amerika</NavDropdown.Item>
-                <NavDropdown.Item href="#korea">Thailand</NavDropdown.Item>
-                <NavDropdown.Item href="#korea">Filiphina</NavDropdown.Item>
-                <NavDropdown.Item href="#korea">Taiwan</NavDropdown.Item>
-              </NavDropdown>
+            {/* Search Button */}
+            <Button
+              variant="link"
+              onClick={handleToggle}
+              className="p-0 custom-toggler"
+              aria-controls="top-navbar-nav"
+              aria-expanded={show}
+            >
+              <FontAwesomeIcon
+                icon={faMagnifyingGlass}
+                className="text-white"
+              />
+            </Button>
+          </div>
+
+          {/* Navbar Collapse */}
+          <Navbar.Collapse id="top-navbar-nav" className={show ? "show" : ""}>
+            <Nav className="me-auto d-flex align-items-center">
+              <Navbar.Brand className="me-3 d-none d-lg-flex align-items-center">
+                <Image
+                  src={config.logo_png}
+                  width={30}
+                  height={30}
+                  loading="lazy"
+                  className="d-inline-block align-top img_contain"
+                />
+                {config.short_name}
+              </Navbar.Brand>
+              <Nav className="d-none d-lg-flex">
+                <Nav.Link href="#indonesia" className="navbar-custom">
+                  Indonesia
+                </Nav.Link>
+                <Nav.Link href="#japan" className="navbar-custom">
+                  Japan
+                </Nav.Link>
+                <Nav.Link href="#korea" className="navbar-custom">
+                  Korea
+                </Nav.Link>
+                <Nav.Link href="#spanyol" className="navbar-custom">
+                  Spanyol
+                </Nav.Link>
+                <Nav.Link href="#inggris" className="navbar-custom">
+                  Inggris
+                </Nav.Link>
+                <Nav.Link href="#amerika" className="navbar-custom">
+                  Amerika
+                </Nav.Link>
+                <Nav.Link href="#thailand" className="navbar-custom">
+                  Thailand
+                </Nav.Link>
+                <Nav.Link href="#filiphina" className="navbar-custom">
+                  Filiphina
+                </Nav.Link>
+                <Nav.Link href="#taiwan" className="navbar-custom">
+                  Taiwan
+                </Nav.Link>
+              </Nav>
+              <Form className="d-flex ms-auto my-2 my-lg-0">
+                <Form.Control
+                  type="text"
+                  placeholder="Search"
+                  aria-label="Search"
+                  className="me-2"
+                  style={{ maxWidth: "270px" }}
+                />
+                <Button variant="outline-light">
+                  <FontAwesomeIcon icon={faMagnifyingGlass} />
+                </Button>
+              </Form>
             </Nav>
-          </Navbar.Collapse> */}
+          </Navbar.Collapse>
         </Container>
       </Navbar>
 
       {/* Navbar Bawah */}
-      <Navbar sticky="top" expand="lg" className="navbar-custom">
+      <Navbar sticky="top" expand="lg" className="navbar-custom mt-2">
+        <Navbar.Toggle
+          aria-controls="bottom-navbar-nav"
+          className="custom-toggler d-lg-none"
+        />
+        {/* Navbar Toggle for Mobile */}
         <Container>
-          {/* Navbar Toggle for Mobile */}
-          <Navbar.Toggle
-            aria-controls="bottom-navbar-nav"
-            className="d-lg-none mx-auto"
-          />
           <Navbar.Collapse id="bottom-navbar-nav" className="text-center">
             <Nav className="justify-content-center">
-              <NavDropdown
-                title="Countries"
-                id="countries-dropdown"
-                className="d-lg-none"
-              >
+              <NavDropdown title="Countries" id="countries-dropdown">
                 <NavDropdown.Item href="#indonesia">Indonesia</NavDropdown.Item>
                 <NavDropdown.Item href="#japan">Japan</NavDropdown.Item>
                 <NavDropdown.Item href="#korea">Korea</NavDropdown.Item>
-                <NavDropdown.Item href="#korea">Spanyol</NavDropdown.Item>
-                <NavDropdown.Item href="#korea">Inggris</NavDropdown.Item>
-                <NavDropdown.Item href="#korea">Amerika</NavDropdown.Item>
-                <NavDropdown.Item href="#korea">Thailand</NavDropdown.Item>
-                <NavDropdown.Item href="#korea">Filiphina</NavDropdown.Item>
-                <NavDropdown.Item href="#korea">Taiwan</NavDropdown.Item>
+                <NavDropdown.Item href="#spanyol">Spanyol</NavDropdown.Item>
+                <NavDropdown.Item href="#inggris">Inggris</NavDropdown.Item>
+                <NavDropdown.Item href="#amerika">Amerika</NavDropdown.Item>
+                <NavDropdown.Item href="#thailand">Thailand</NavDropdown.Item>
+                <NavDropdown.Item href="#filiphina">Filiphina</NavDropdown.Item>
+                <NavDropdown.Item href="#taiwan">Taiwan</NavDropdown.Item>
               </NavDropdown>
-
-              <Navbar.Brand>Filter by:</Navbar.Brand>
 
               <NavDropdown title="Year" id="year-dropdown" className="me-2">
                 <NavDropdown.Item href="#2024">2024</NavDropdown.Item>

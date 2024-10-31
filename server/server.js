3d021cd1b76fdbdaa0194bf209f4dfbdc6fef1e4
aryrk <amanwhohave@gmail.com>
2024-10-31 12:39:12 +0700

    [fix] fix redis

--- server/server.js
+++ server/server.js
@@ -20,28 +20,6 @@ require("dotenv").config();
 // const port = 5000;
 // const domain = "http://localhost:" + port;
 
-app.set("trust proxy", 1);
-
-app.use(
-  session({
-    cookie: {
-      secure: true,
-      maxAge: 60000,
-    },
-    store: new RedisStore(),
-    secret: "secret",
-    saveUninitialized: true,
-    resave: false,
-  })
-);
-
-app.use(function (req, res, next) {
-  if (!req.session) {
-    return next(new Error("Oh no")); //handle error
-  }
-  next(); //otherwise continue
-});
-
 const client_domain = process.env.CLIENT_URL;
 const port = process.env.SERVER_PORT;
 const domain = process.env.SERVER_URL;
